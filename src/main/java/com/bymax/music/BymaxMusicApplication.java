94f10cbcd27359f8670b52de26b228b50e12aa57
alimzhan <15365185687@qq.com>
2022-04-03 21:16:30 +0800

    hello world in springboot

---
+++ src/main/java/com/bymax/music/BymaxMusicApplication.java
@@ -0,0 +1,13 @@
+package com.bymax.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BymaxMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BymaxMusicApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/bymax/music/controller/DefaultController.java
@@ -0,0 +1,15 @@
+package com.bymax.music.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class DefaultController {
+
+    @GetMapping
+    public String sayHello() {
+        return "hello wold!";
+    }
+}
